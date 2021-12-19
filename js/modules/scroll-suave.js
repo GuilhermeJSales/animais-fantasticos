42937edecfab0204f04f2b83113558b436f66d8e
Guilherme Jesus Sales <guijsalesdg@outlook.com>
2021-12-19 16:43:56 -0300

    Scroll Suave transformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,17 +1,31 @@
-export default function scrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="scroll-suave"] a[href^="#"]');
-  function suaveScroll(event) {
+export default class ScrollSuave {
+  constructor(links, options){
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) { 
+      this.options = { behavior: 'smooth', block: 'start', };  
+    } else {
+      this.options = options;
+    }
+    this.scrollToSection = this.scrollToSection.bind(this)
+  }
+  
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const sections = document.querySelector(href);
-    sections.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+    sections.scrollIntoView(this.options);
+  }
+
+  adicionarEvento(){
+    this.linksInternos.forEach((links) => {
+      links.addEventListener('click', this.scrollToSection);
     });
   }
-  if (linksInternos.length) {
-    linksInternos.forEach((links) => {
-      links.addEventListener('click', suaveScroll);
-    });
+
+  init(){
+    if (this.linksInternos.length){
+    this.adicionarEvento();
+  }
+    return this;   
   }
 }
