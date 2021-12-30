6921d60f44e73215e4235ec105c6c578ff1476cc
Guilherme Jesus Sales <guijsalesdg@outlook.com>
2021-12-30 13:33:17 -0300

    adicionado script build na pasta dist

--- js/script.js
+++ js/script.js
@@ -41,9 +41,6 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto')
 funcionamento.init();
 
-
-
-
 fetchAnimais('../../animaisapi.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
